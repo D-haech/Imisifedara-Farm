01804fcf4b4e9031941b26ba3331e5341fb290c1
D-haech <osarogiedanieligbinosa@gmail.com>
2024-12-23 14:33:48 +0100

    Arranging the home page

--- imisifedara/src/pages/Home.jsx
+++ imisifedara/src/pages/Home.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from "react";
+import Hero, { Display, Footer, Reliance } from "../components/Hero";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import pig from "../assets/images/pig116.jpg";
+import fish from "../assets/images/images/fish.png"
+import pig2 from "../assets/images/images/pig112.jpg"
+import { useEffect } from "react";
 
 const Home = () => {
+useEffect(() =>{document.title =`Imisifedara`})
   return (
     <div>
+      <div className="row">
+        <Hero></Hero>
+      </div>
+      <div className="row">
+        <Display image={fish} heading={`Raw is war`} detail={"Let us resume the war"}></Display>
+        <Display image={pig2}/>
+      </div>
 
-      <button onClick={() => console.log('click me')}> click me</button>
+      <Row>
+        <Reliance></Reliance>
+      </Row>
+
+      <Row>
+        <Footer></Footer>
+      </Row>
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
