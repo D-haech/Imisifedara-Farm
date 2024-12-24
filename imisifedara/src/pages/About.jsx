ab2be8c2b011ca0e1a02e28c1e311c9ec903a503
D-haech <osarogiedanieligbinosa@gmail.com>
2024-12-24 15:44:59 +0100

    title bar is working

--- imisifedara/src/pages/About.jsx
+++ imisifedara/src/pages/About.jsx
@@ -1,6 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { useEffect, useRef } from 'react';
+
 
 const About = () => {
+  const getRef = useRef(0)
+  
+  useEffect(()=>{
+    document.title = `About Us`
+  }, [getRef.current]);
   
   return (
     <div>About</div>
--- imisifedara/src/pages/More.jsx
+++ imisifedara/src/pages/More.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useEffect, useRef } from 'react'
+
 
 const More = () => {
+const getRef = useRef(0)
+  
+  useEffect(()=>{
+    document.title = `More`
+  }, [getRef.current])
   return (
     <div>More</div>
   )
