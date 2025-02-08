568e8d13ef3bb95a28c39f23de7384d925eb509e
D-haech <osarogiedanieligbinosa@gmail.com>
2025-02-08 07:44:02 +0100

    update build.sh

--- Back-End/imisi_proj/build.sh
+++ Back-End/imisi_proj/build.sh
@@ -1,6 +1,8 @@
+cd Back-end/imisi_proj
+#!/bin/bash
+cd $(dirname "$0") 
 set -o errexit #this is set to test the file
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-poetry install
